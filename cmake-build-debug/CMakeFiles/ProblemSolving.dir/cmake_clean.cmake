664fa2d805feb1f4fa341b73f09482693f991208
zzzeeshannn <zeeshanshaikh1205@gmail.com>
2021-12-20 05:22:07 -0500

    New Update

---
+++ cmake-build-debug/CMakeFiles/ProblemSolving.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProblemSolving.dir/main.cpp.o"
+  "ProblemSolving"
+  "ProblemSolving.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProblemSolving.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ProblemSolving.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ProblemSolving.
+# This may be replaced when dependencies are built.
